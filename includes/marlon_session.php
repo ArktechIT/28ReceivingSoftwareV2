199b18063ba66f8dc438715eaa996975c3b3f8b2
Marlon Mercado <marlonilaganmercado@gmail.com>
2022-04-05 08:39:05 +0800

    user profile session

---
+++ includes/marlon_session.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    
+    if(!isset($_SESSION['idNumber']) || empty($_SESSION['idNumber'])){
+       	header("Location: ../../../val_login.php");
+        exit();
+    }
+?>
